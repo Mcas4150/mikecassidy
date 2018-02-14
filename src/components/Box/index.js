3e47bf886503f9ad72bc98708a7a7981757ce909
Michael Cassidy <mikecassidy0000@gmail.com>
2018-02-14 18:34:28 -0500

    skills

--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -43,6 +43,11 @@ componentDidMount() {
 }
 
 
+
+
+
+
+
   export default Box;
 
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -5,28 +5,51 @@ import './Sidebar.css';
 
 
 
+
+
+      function sidebarTransition() {
+        let el = document.querySelector("div.sidebar");
+
+        if (el) {
+          el.className = "sidebar-full";
+        } else {
+          el = document.querySelector("div.sidebar-full");
+          el.className = "sidebar";
+        }
+
+        return el;
+      };
+
+
+
+
+
+
+
 class Sidebar extends Component {
   render() {
     return (
         <div className="sidebar">
+
 {/*          <Link onlyActiveOnIndex={true} key={1} to="/" activeClassName="activeNavLink" className="navLink">
               <h1>Mike Cassidy</h1>
             </Link>*/}
             <h1>Mike Cassidy</h1>
           <h4>Full-Stack Web Developer</h4>
           <div className="links">
-              <NavLink  to="/portfolio" activeClassName="activeNavLink" className="navLink">
+              <NavLink  to="/portfolio" activeClassName="activeNavLink" onClick={sidebarTransition} className="navLink">
                 Portfolio
               </NavLink>
-              <NavLink  to="/skills" activeClassName="activeNavLink" className="navLink">
+              <NavLink  to="/skills" activeClassName="activeNavLink" onClick={sidebarTransition} className="navLink">
                 Skills
               </NavLink>
-              <NavLink  to="/contact" activeClassName="activeNavLink" className="navLink">
+              <NavLink  to="/contact" activeClassName="activeNavLink" onClick={sidebarTransition} className="navLink">
                 Contact
               </NavLink>
 
-          </div>
-          <Box/>
+            </div>
+
+
         </div>
     );
   }
