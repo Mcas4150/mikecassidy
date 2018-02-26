7062aca7cfd611b4a574805896450e010d017000
Michael Cassidy <mikecassidy0000@gmail.com>
2018-02-26 17:35:44 -0500

    links

--- src/components/Bio/index.js
+++ src/components/Bio/index.js
@@ -20,7 +20,7 @@ class Bio extends Component {
 I’m currently living in Boston, Massachusetts. Previously, I've worked with autistic children as an applied behavioral analyst and at the music distributor <a href="https://www.forcedexposure.com/home.html">Forced Exposure</a>. I am now specializing in Web Development: how they break, the teams that maintain them, and how they work at scale and across mediums.
         </p>
         <p>
-I spend my time <a href="">listening</a>, adapting, building electronics, and writing <a href="https://github.com/Mcas4150">code</a>. I also write about design, courage, and lack thereof.
+I spend my time <a href="">listening</a>, adapting, building electronics, and writing <a href="https://github.com/Mcas4150">code</a>.
 
 I’m currently available for work, so please take a look at  <Link to={`/skills`}>my résumé</Link> or send me an <a href="mailto:mikecassidy0000@gmail.com">email</a> if you would like to be in touch.
         </p>
