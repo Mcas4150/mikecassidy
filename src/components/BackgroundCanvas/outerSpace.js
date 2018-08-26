fab5369208c59aa87ac5b664eaeaf3dca7a3e002
Michael Cassidy <mikecassidy0000@gmail.com>
2018-08-25 22:02:41 -0400

    mike

--- src/components/BackgroundCanvas/outerSpace.js
+++ src/components/BackgroundCanvas/outerSpace.js
@@ -40,7 +40,7 @@ export default class outerSpace extends Component {
 
     function draw() {
       // This just clears the screen
-      context.fillStyle = "255";
+      context.fillStyle = "rgb(255, 255, 255)";
       context.fillRect(0, 0, width, height);
 
       // This loop takes a bunch of 3D vertices and draws them using a 2D perspective projection
