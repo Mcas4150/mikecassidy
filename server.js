4eeecf2cf5ff8b98e93fd40a25ead7f8ba563d19
Michael Cassidy <mikecassidy0000@gmail.com>
2018-09-30 11:44:27 -0400

    setup sll for https

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  if (!req.secure && req.headers["x-forwarded-proto"] !== "https") {
+    res.redirect("https://" + req.headers.host + req.url);
+  } else {
+    next();
+  }
+});
+
 app.use(express.static(path.resolve(__dirname + "/build")));
 
 app.get("*", (req, res) => {
