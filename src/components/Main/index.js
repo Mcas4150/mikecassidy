934b735469277c5fc8f1fca655a744b3fd08e1c4
Mcas4150 <mikecassidy0000@gmail.com>
2019-07-22 12:50:48 -0400

    skills

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import "./Main.css";
 import { Switch, Route } from "react-router-dom";
-import Skills from "../Skills/index";
 import Contact from "../Contact/index";
 import Bio from "../Bio/index";
 import Portfolio from "../Portfolio/index";
@@ -14,7 +13,7 @@ class Main extends Component {
         <Switch>
           <Route exact path="/" component={Bio} />
           <Route path="/portfolio" component={Portfolio} />
-          <Route path="/skills" component={Skills} />
+
           <Route path="/contact" component={Contact} />
         </Switch>
       </main>
